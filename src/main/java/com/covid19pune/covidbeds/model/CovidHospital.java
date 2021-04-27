9b4e5d6bb39f10a7c7b25b8ab69969b032a91d3e
SudhirKulat <sudhir.kulat@gmail.com>
2021-04-27 18:59:28 +0530

    Add Hospital bed details.

--- src/main/java/com/covid19pune/covidbeds/model/CovidHospital.java
+++ src/main/java/com/covid19pune/covidbeds/model/CovidHospital.java
@@ -3,6 +3,17 @@ package com.covid19pune.covidbeds.model;
 public class CovidHospital {
     private String hospitalName;
     private String hospitalAddress;
+    private String number;
+    private String bedCapacity;
+    private String availableBedWithoutOxy;
+    private String vacantBedWithoutOxy;
+    private String AllocatedBedWithOxy;
+    private String vacantBedWithOxy;
+    private String allocatedBedIcuWithouVenti;
+    private String vacantBedIcuWithoutVenti;
+    private String allocatedBedWithVenti;
+    private String vacantBedIcuWithVenti;
+
 
 
     public String getHospitalName() {
@@ -19,6 +30,86 @@ public class CovidHospital {
 
     public void setHospitalAddress(String hospitalAddress) {
         this.hospitalAddress = hospitalAddress;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getBedCapacity() {
+        return bedCapacity;
+    }
+
+    public void setBedCapacity(String bedCapacity) {
+        this.bedCapacity = bedCapacity;
+    }
+
+    public String getAvailableBedWithoutOxy() {
+        return availableBedWithoutOxy;
+    }
+
+    public void setAvailableBedWithoutOxy(String availableBedWithoutOxy) {
+        this.availableBedWithoutOxy = availableBedWithoutOxy;
+    }
+
+    public String getVacantBedWithoutOxy() {
+        return vacantBedWithoutOxy;
+    }
+
+    public void setVacantBedWithoutOxy(String vacantBedWithoutOxy) {
+        this.vacantBedWithoutOxy = vacantBedWithoutOxy;
+    }
+
+    public String getAllocatedBedWithOxy() {
+        return AllocatedBedWithOxy;
+    }
+
+    public void setAllocatedBedWithOxy(String allocatedBedWithOxy) {
+        this.AllocatedBedWithOxy = allocatedBedWithOxy;
+    }
+
+    public String getVacantBedWithOxy() {
+        return vacantBedWithOxy;
+    }
+
+    public void setVacantBedWithOxy(String vacantBedWithOxy) {
+        this.vacantBedWithOxy = vacantBedWithOxy;
+    }
+
+    public String getAllocatedBedIcuWithouVenti() {
+        return allocatedBedIcuWithouVenti;
+    }
+
+    public void setAllocatedBedIcuWithouVenti(String allocatedBedIcuWithouVenti) {
+        this.allocatedBedIcuWithouVenti = allocatedBedIcuWithouVenti;
+    }
+
+    public String getVacantBedIcuWithoutVenti() {
+        return vacantBedIcuWithoutVenti;
+    }
+
+    public void setVacantBedIcuWithoutVenti(String vacantBedIcuWithoutVenti) {
+        this.vacantBedIcuWithoutVenti = vacantBedIcuWithoutVenti;
+    }
+
+    public String getAllocatedBedWithVenti() {
+        return allocatedBedWithVenti;
+    }
+
+    public void setAllocatedBedWithVenti(String allocatedBedWithVenti) {
+        this.allocatedBedWithVenti = allocatedBedWithVenti;
+    }
+
+    public String getVacantBedIcuWithVenti() {
+        return vacantBedIcuWithVenti;
+    }
+
+    public void setVacantBedIcuWithVenti(String vacantBedIcuWithVenti) {
+        this.vacantBedIcuWithVenti = vacantBedIcuWithVenti;
     }
 
     @Override
--- src/main/java/com/covid19pune/covidbeds/service/BedService.java
+++ src/main/java/com/covid19pune/covidbeds/service/BedService.java
@@ -18,14 +18,25 @@ public class BedService {
 
             List<CovidHospital> ch = new ArrayList<>();
             Document document = Jsoup.connect(url).get();
-            List<Element> element = document.select("#tablegrid tr td:eq(4)");
+            List<Element> element = document.select("table:eq(2) tr[class]");
+
             element.forEach(e ->{
                     CovidHospital covidHospital = new CovidHospital();
-                    covidHospital.setHospitalName(e.select("b:eq(0)").text());
-                    if(e.select("b:eq(2)").size() > 0){
-                        covidHospital.setHospitalAddress(e.select("b:eq(2)").get(0).nextSibling().toString());
+                    covidHospital.setHospitalName(e.select("td:eq(4) b").first().text());
+                    if(e.select("td:eq(4) b:eq(2)").size() > 0){
+                        covidHospital.setHospitalAddress(e.select("td:eq(4) b:eq(2)").get(0).nextSibling().toString());
+                        covidHospital.setNumber(e.select("td:eq(4) b:eq(4)").get(0).nextSibling().toString());
                     }
 
+                    covidHospital.setBedCapacity(e.select("td:eq(8)").text());
+                    covidHospital.setAvailableBedWithoutOxy(e.select("td:eq(9)").text());
+                    covidHospital.setVacantBedWithoutOxy(e.select("td:eq(10)").text());
+                    covidHospital.setAllocatedBedWithOxy(e.select("td:eq(11)").text());
+                    covidHospital.setVacantBedWithOxy(e.select("td:eq(12)").text());
+                    covidHospital.setAllocatedBedIcuWithouVenti(e.select("td:eq(13)").text());
+                    covidHospital.setVacantBedIcuWithoutVenti(e.select("td:eq(14)").text());
+                    covidHospital.setAllocatedBedWithVenti(e.select("td:eq(15)").text());
+                    covidHospital.setVacantBedIcuWithVenti(e.select("td:eq(16)").text());
                     ch.add(covidHospital);
             });
 
